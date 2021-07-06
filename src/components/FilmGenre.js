67edf9cc4c2d6c00ea706f8a5f6349c5df784c1e
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-07-05 20:07:05 -0400

    Movie_app created, styled-components installed and components was created

---
+++ src/components/FilmGenre.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilmGenre = () => {
+	return (
+		<div>
+			<h1>film genre</h1>
+		</div>
+	);
+};
+
+export default FilmGenre;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+	return (
+		<div>
+			<h1>Genre title</h1>
+		</div>
+	);
+};
+
+export default Title;
